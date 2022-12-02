1e9bc6b378576722555c6dcbfcdeacff02e3981e
SaverioCascelli <saverio.cascelli@gmail.com>
2022-12-02 16:13:19 +0100

    init

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+function genPassword($pswLength){
+    $specials= '!?&%$<>^+-*/()[]{}@#_=)';
+    $numbers= '0123456789';
+    $char = 'abcdefghijklmnopqrstuvwxyz';
+    $capitalChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $pswArrChar = $specials . $numbers . $char . $capitalChar;
+
+
+}
